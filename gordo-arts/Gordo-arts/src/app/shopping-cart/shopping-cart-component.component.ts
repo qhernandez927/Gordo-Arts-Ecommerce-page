16231cb42660057c59d10eb684df5fae88829408
qhernandez927 <quinnh2791@gmail.com>
2020-06-07 21:52:05 -0500

    Added paypal check out per item

--- gordo-arts/Gordo-arts/src/app/shopping-cart/shopping-cart-component.component.ts
+++ gordo-arts/Gordo-arts/src/app/shopping-cart/shopping-cart-component.component.ts
@@ -1,31 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import {CartService} from '../cart.service'
 import {Observable} from 'rxjs'
 import { ISticker } from '../classes/isticker';
+import { StickerComponent } from '../sticker/sticker.component';
+import { async } from 'q';
 
 
 @Component({
-  selector: 'app-shopping-cart-component',
-  template: `<p *ngIf="shoppingCartItems$|async as items">
-  You have {{items.length}} items in your bag
+  selector: 'shoppingcart',
+  template: `
+  <p *ngIf="shoppingCartItems$|async as stickers">
+  You have {{stickers.length}} items in your bag
 </p>
-<sticker *ngFor="let sticker of (shoppingCartItems$|async)" [sticker]="sticker"></sticker>`,
+<sticker *ngFor="let sticker of (shoppingCartItems$|async)" [sticker]="sticker">
+</sticker>
+<p>Total: {{getTotal() | async}}</p>
+`,
+
   styleUrls: ['./shopping-cart-component.component.css']
 })
 export class ShoppingCartComponentComponent implements OnInit {
+
 
   public shoppingCartItems$: Observable<ISticker[]>;
 
   public shoppingCartItems: ISticker[] = [];
   
-
+  
+   
   
   constructor(private cartService: CartService) { 
   }
 
   async ngOnInit() {
-    this.shoppingCartItems$ = this.cartService.getItems()
-      
+    this.shoppingCartItems$ = this.cartService.getItems();
   }
 
   
--- gordo-arts/Gordo-arts/src/app/sticker/sticker.component.ts
+++ gordo-arts/Gordo-arts/src/app/sticker/sticker.component.ts
@@ -1,10 +1,15 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ElementRef, ViewChild, OnInit } from '@angular/core';
+import { ISticker } from '../classes/isticker';
+import {CartService} from '../cart.service'
+import { ShoppingCartComponentComponent } from '../shopping-cart/shopping-cart-component.component';
+
+declare var paypal;
+
 
 @Component({
   selector: 'sticker',
-  template: `<div class="card-body">
-  <div class="row">
-  <div class="col-md-3 col-sm-6">
+  template: 
+      `<div class="col-md-3 col-sm-6">
       <figure class="card card-product-grid mt-5">
           <div class="img-wrap">
               <img class="card-img-top img-fgr img-fluid" src={{sticker.imgPath}}>
@@ -13,19 +18,61 @@ import { Component, Input } from '@angular/core';
               <p  class="title text-left ml-1">{{sticker.description}}</p>
               <div class="price mt-2">{{sticker.price}}</div>
           </figcaption>
-      </figure> 
-  </div> 
-  </div>
-</div> `
+      </figure>
+      <button class="remove-item" (click)="removeItem(sticker)">Remove</button>
+      </div>
+      <div #paypal></div>
+      `
 
 })
-export class StickerComponent {
+export class StickerComponent implements OnInit{
     @Input() sticker:  
     {id: number;
     description: string;
     price: number;
     imgPath: string;};
-  
 
+    @ViewChild('paypal', { static: true }) paypalElement: ElementRef;
+    
+    paidFor = false;
+
+
+
+     constructor(private cartService: CartService,
+                private userTotal: ShoppingCartComponentComponent) { 
+    }
+  
+    public removeItem(sticker: ISticker) {
+        this.cartService.removeFromCart(sticker);
+      }
+    
+
+      ngOnInit() {
+        paypal
+          .Buttons({
+            createOrder: (data, actions) => {
+              return actions.order.create({
+                purchase_units: [
+                  {
+                    description: this.sticker.description,
+                    amount: {
+                      currency_code: 'USD',
+                      value: this.sticker.price,
+                    }
+                  }
+                ]
+              });
+            },
+            onApprove: async (data, actions) => {
+              const order = await actions.order.capture();
+              this.paidFor = true;
+              console.log(order);
+            },
+            onError: err => {
+              console.log(err);
+            }
+          })
+          .render(this.paypalElement.nativeElement);
+      }
     
 }
